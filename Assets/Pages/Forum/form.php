ae6d976b00519651253ceb4bb9c0541f93c8e5db
DulanjaleeDK <dulanjaleedinushi99@gmail.com>
2024-06-05 20:26:48 +0530

    create topic

---
+++ Assets/Pages/Forum/form.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+if (isset($_SESSION['username']) || isset($_SESSION['role'])) {
+    $username = $_SESSION['username'];
+    $role = $_SESSION['role'];
+    if ($role != 'Innovator') {
+        echo "<script>window.location.href='../../../index.php';</script>";
+        exit();
+    }
+} else {
+    // header("Location: ../../../index.php");
+    echo "<script>window.location.href='../../../index.php';</script>";
+    exit();
+}
+
+include '../dbconnection.php';
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<?php include 'innovator-nav.php'; ?>
+
+<body class="bg-dark text-white">
+
+<h1>hello</h1>
+    
+   
+    
+</body>
+</html
