83e2dc8daa8661c6bc98e59043aadce477e66d69
DulanjaleeDK <dulanjaleedinushi99@gmail.com>
2024-06-06 12:09:23 +0530

    General Information

--- Assets/Pages/Forum/form.php
+++ Assets/Pages/Forum/form.php
@@ -13,6 +13,7 @@ if (isset($_SESSION['username']) || isset($_SESSION['role'])) {
     exit();
 }
 
+
 include '../dbconnection.php';
 
 ?>
@@ -24,14 +25,77 @@ include '../dbconnection.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        html 
+        {font-size: 14px;
+        font-family: 'Titilium Web',sans-serif;
+        background-color:black;
+        color:white;
+        }
+
+
+        
+      </style>
 </head>
-<?php include 'innovator-nav.php'; ?>
+<!-- <?php include 'innovator-nav.php'; ?> -->
 
 <body class="bg-dark text-white">
 
-<h1>hello</h1>
-<h2>hello world </h2>
-    
+
+    <div class="container">
+          <div class="subforum">
+             <div class="subforum-title">
+               <h1> General Information</h1>
+             </div>
+             <div class="subforum-row">
+                    <div class="subforum-icon">
+                        <i class="fa fa-car"></i>
+
+                    </div>
+                    <div class="subforum-description">
+                       <h1><a href="">=Description Title:</a></h1>
+                        <p>Description content</p>
+                    </div>
+                    <div class="subforum-stats">
+                     <span>24 posts | 15 Topics</span>
+                
+                    </div>
+                    <div class="subform-info">
+                        <b><a href="">Last post</a></b> <a href="">JustUser</a>
+                        <br>
+                        on <small>22 Aug 2024</small>
+                         
+                    </div>
+                     
+                    
+
+
+
+             </div>
+            
+            
+            
+            
+            
+            
+            </div>
+        
+        
+        
+        
+        
+        
+        
+        
+        </div>
+
+
+
+
+
+
+
+</div>
    
     
 </body>
