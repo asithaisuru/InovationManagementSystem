f12f9352a18b101b6801eabeea8bcd21856be69f
Chithran Jayathunga <106954219+Chithranjaye2000@users.noreply.github.com>
2024-06-16 19:40:59 +0530

    Save the liked post ids to local storage after the page refreshes

--- Assets/Pages/Forum/forum.php
+++ Assets/Pages/Forum/forum.php
@@ -185,7 +185,6 @@ include '../dbconnection.php';
                     localStorage.setItem('likedPosts', JSON.stringify(likedPosts));
                 });
             });
-
         
     </script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
